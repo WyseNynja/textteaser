d8217f5c6bfb154ec9a4558820aae0a7c0339a61
Bryan Stitt <bryan@stitthappens.com>
2016-05-28 14:54:00 -0700

    use setup.py instead of MANIFEST.in

--- setup.py
+++ setup.py
@@ -3,17 +3,22 @@
 import setuptools
 
 setuptools.setup(
-    name='textteaser',
-    version='1.0.0',
     description=__doc__,
-    packages=setuptools.find_packages(),
-    install_requires=[
-        'nltk',
-    ],
     entry_points={
         'console_scripts': [
             'textteaser-cli = textteaser.main:main',
             'textteaser-loop = textteaser.main:loop_main',
         ],
     },
+    include_package_data=True,
+    install_requires=[
+        'nltk',
+    ],
+    name='textteaser',
+    package_data={
+        # If any package contains *.txt or *.pickle files, include them:
+        'textteaser': ['trainer/*'],
+    },
+    packages=setuptools.find_packages(),
+    version='1.0.0',
 )
