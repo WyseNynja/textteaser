df4e0b273f6f61c1c9ffac3848db008a550589ab
Bryan Stitt <bryan@stitthappens.com>
2016-05-28 13:21:56 -0700

    simpler getSentencePositionScore

--- textteaser/parser.py
+++ textteaser/parser.py
@@ -27,25 +27,27 @@ class Parser:
     def getSentencePositionScore(self, i, sentenceCount):
         normalized = i / (sentenceCount * 1.0)
 
-        if normalized > 0 and normalized <= 0.1:
+        if normalized <= 0:
+            return 0
+        elif normalized <= 0.1:
             return 0.17
-        elif normalized > 0.1 and normalized <= 0.2:
+        elif normalized <= 0.2:
             return 0.23
-        elif normalized > 0.2 and normalized <= 0.3:
+        elif normalized <= 0.3:
             return 0.14
-        elif normalized > 0.3 and normalized <= 0.4:
+        elif normalized <= 0.4:
             return 0.08
-        elif normalized > 0.4 and normalized <= 0.5:
+        elif normalized <= 0.5:
             return 0.05
-        elif normalized > 0.5 and normalized <= 0.6:
+        elif normalized <= 0.6:
             return 0.04
-        elif normalized > 0.6 and normalized <= 0.7:
+        elif normalized <= 0.7:
             return 0.06
-        elif normalized > 0.7 and normalized <= 0.8:
+        elif normalized <= 0.8:
             return 0.04
-        elif normalized > 0.8 and normalized <= 0.9:
+        elif normalized <= 0.9:
             return 0.04
-        elif normalized > 0.9 and normalized <= 1.0:
+        elif normalized <= 1.0:
             return 0.15
         else:
             return 0
